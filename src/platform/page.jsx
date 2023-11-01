addd1d2327ac0f1bc868d37e9f615a9c94b5cc92
Anoop <anoop@webenertia.com>
2023-11-01 19:53:07 +0530

    add two new pages

---
+++ src/platform/page.jsx
@@ -0,0 +1,7 @@
+export default function Platform() {
+    return (
+      <>
+        <h1 className="text-center py-16">Platform</h1>
+      </>
+    )
+  }
---
+++ src/solutions/page.jsx
@@ -0,0 +1,7 @@
+export default function Solution() {
+    return (
+      <>
+        <h1 className="text-center py-16">Solutions</h1>
+      </>
+    )
+  }
