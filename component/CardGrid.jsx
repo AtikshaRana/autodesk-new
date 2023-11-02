4a764c8413f6783070b6626fcad8b3e7f6559693
Anoop <anoop@webenertia.com>
2023-11-02 19:56:15 +0530

    update section

--- component/CardGrid.jsx
+++ component/CardGrid.jsx
@@ -67,13 +67,13 @@ function CardGrid() {
                         <div className="wrapper flex threeCardWrap flex-wrap">
                         {smallcards.map((item, i) => {
                            return(<div key={i} className="small_card w-halfWidth rounded-lg mx-[10px] mb-3 p-4 flex flex-wrap bg-black text-white">
-                               <div className="num_wrap  w-1/5">
-                                   <div className="counter my-auto pt-5 text-right relative">
+                               <div className="num_wrap  w-3/12">
+                                   <div className="counter relative w-[60px] h-[60px] bg-yello rounded-full  my-auto  text-right relative">
                                        
-                                   <span className='text-white relative  inline rounded-full z-10 font-bold text-xl'> <div className='absolute -top-5 -left-3 w-[60px] h-[60px] rounded-full bg-yello -z-[1]'></div>{ item.number}</span>
+                                       <span className='text-black absolute left-1/2 top-1/2 -translate-y-2/4 -translate-x-2/4  inline  z-10 font-bold text-xl'> {item.number}</span>
                                    </div>
                                </div>
-                               <div className="text-wrap pl-8 w-4/5">
+                               <div className="text-wrap pl-4 w-9/12">
                                    <h6>{ item.title}</h6>
                                    <Link href='/' className='text-xs'>{ item.linkText}</Link>
                                 </div>
