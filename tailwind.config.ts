95dcdf511fa8e450685ee181f4a59e83b412c1be
Atiksha <atiksha@webenertia.com>
2023-11-04 17:16:09 +0530

    Update js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,7 +33,67 @@ const config: Config = {
         'threeCardWrap':'calc(100% + 20px)',
         'threeCard': 'calc(33.33% - 20px)',
         'halfWidth':'calc(50% - 20px)'
-      }
+      },
+      screens: {
+        'bcd': {'max': '1399px'},
+        // => @media (max-width: 1399px) { ... }
+        'bcu': {'min': '1399px'},
+        // => @media (max-width: 1399px) { ... }
+
+        'elcd': {'max': '1399px'},
+        // => @media (max-width: 1399px) { ... }
+        'elcu': {'min': '1399px'},
+        // => @media (max-width: 1399px) { ... }
+
+        'ldd': {'max': '1270px'},
+        // => @media (max-width: 1270px) { ... }
+        'ldu': {'min': '1270px'},
+        // => @media (max-width: 1270px) { ... }
+
+        'cd': {'max': '1170px'},
+        // => @media (max-width: 1279px) { ... }
+        'cu': {'min': '1170px'},
+        // => @media (min-width: 1279px) { ... }
+
+        'dmd': {'max': '1024px'},
+        // => @media (max-width: 1024px) { ... }
+        'dmu': {'min': '1024px'},
+        // => @media (min-width: 1024px) { ... }
+
+        'desktop': {'min': '992px', 'max': '1024px'},
+        // => @media (min-width: 992px and max-width: 1024px) { ... }
+
+        'dd': {'max': '991px'},
+        // => @media (max-width: 991px) { ... }
+        'du': {'min': '991px'},
+        // => @media (min-width: 991px) { ... }
+
+        'tablet': {'min': '768px', 'max': '991px'},
+        // => @media (min-width: 768px and max-width: 991px) { ... }
+
+        'td': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+        'tu': {'min': '767px'},
+        // => @media (min-width: 767px) { ... }
+
+        'phablet': {'min': '596px', 'max': '767px'},
+        // => @media (min-width: 596px and max-width: 767px) { ... }
+
+        'phd': {'max': '595px'},
+        // => @media (max-width: 595px) { ... }
+        'phu': {'min': '595px'},
+        // => @media (min-width: 595px) { ... }
+
+        'phone': {'min': '481px', 'max': '595px'},
+        // => @media (min-width: 481px and max-width: 595px) { ... }
+
+        'pd': {'max': '480px'},
+        // => @media (max-width: 480px) { ... }
+        'pu': {'min': '480px'},
+        // => @media (min-width: 480px) { ... }
+
+      },
+  
     },
     
   },
